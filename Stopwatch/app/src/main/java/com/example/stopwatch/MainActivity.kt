ba328dd7cd718e84fc7286c568f630fb5473b871
jnikd <jnikd0826@gmail.com>
2017-06-11 08:56:44 +0900

    テキスト更新

--- Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.stopwatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import com.example.stopwatch.R.id.time
+import com.example.stopwatch.R.id.timeText
 
 class MainActivity : AppCompatActivity() {
+
+    val handler = Handler()
+    var timeValue = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timeText = findViewById(R.id.timeText) as TextView
+
+        val runnable = object : Runnable {
+            override fun run() {
+                timeValue++
+                timeText.text = timeValue.toString()
+                handler.postDelayed(this, 1000)
+            }
+        }
+        handler.post(runnable)
     }
 }
